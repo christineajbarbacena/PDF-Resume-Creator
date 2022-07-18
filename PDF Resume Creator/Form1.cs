df706105551f0e0a32acc7cfdc822fe88f69e29a
christineajbarbacena <christinebarbacena9@gmail.com>
2022-07-18 23:57:17 +0800

    Last na talaga

--- PDF Resume Creator/Form1.cs
+++ PDF Resume Creator/Form1.cs
@@ -3,7 +3,6 @@ using System.Windows.Forms;
 using System.IO;
 using Newtonsoft.Json;
 using Json.Net;
-using System.Text;
 using System.Text.Json;
 
 namespace PDF_Resume_Creator
@@ -17,11 +16,10 @@ namespace PDF_Resume_Creator
 
         private void bttncnvrt_Click(object sender, EventArgs e)
         {
-            String Resumefile = (@"C:\Users\barbacena\source\repos\PDF Resume Creator\PDF Resume Creator\ResumeInfo.json");
-            String readresume = File.ReadAllText(Resumefile);
-            Infos sresume = JsonSerializer.Deserialize<Infos>(readresume)!;
+            string resumefile = (@"C:\Users\barbacena\source\repos\PDF Resume Creator\PDF Resume Creator\ResumeInfo.json");
+            string readresume = File.ReadAllText(resumefile);
+            Infos inforesume = System.Text.Json.JsonSerializer.Deserialize<Infos>(readresume);
 
-            //Personal Information
 
         }
     }
--- PDF Resume Creator/Infos.cs
+++ PDF Resume Creator/Infos.cs
@@ -16,7 +16,38 @@ namespace PDF_Resume_Creator
         public string EmailAddress { get; set; }
         public string MobileNumber { get; set; }
         public string StreetName { get; set; }
+        public string City { get; set; }
+        public string Province  { get; set; }
+        public string Region { get; set; }
 
+        //skills and objectives
+        public string Objective { get; set; }
+        public string Skill1 { get; set; }
+        public string Skill2 { get; set; }
+        public string Skill3 { get; set; }
+        public string Skill4 { get; set; }
+
+        //work experience
+        public string WorkExperience { get; set; }
+        public string WEAddress { get; set; }
+        public string Position { get; set; }
+        public string WorkYears { get; set; }
+
+        //Educational Background
+        public string Tertiary { get; set; }
+        public string Course { get; set; }
+        public string TertiaryYears { get; set; }
+        public string SeniorHighSchool { get; set; }
+        public string Strand { get; set; }
+        public string SHSAward { get; set; }
+        public string SecondaryYears { get; set; }
+        public string JuniorHighSchool { get; set; }
+        public string JHSAward { get; set; }
+        public string JuniorYears { get; set; }
+        
+        //Awards
+        public string Awards1 { get; set; }
+        public string Awards2 { get; set; }
     }
 
 }
