c9fe02cfbb2f313fb581f10dda779b3269b17d1c
christineajbarbacena <christinebarbacena9@gmail.com>
2022-07-19 00:07:39 +0800

    Found the error :)

--- PDF Resume Creator/Form1.cs
+++ PDF Resume Creator/Form1.cs
@@ -20,7 +20,28 @@ namespace PDF_Resume_Creator
             string readresume = File.ReadAllText(resumefile);
             Infos inforesume = System.Text.Json.JsonSerializer.Deserialize<Infos>(readresume);
 
+            //Converting basic Information
+            string FirstName = inforesume.FirstName;
+            string MiddleName = inforesume.MiddleName;
+            string LastName = inforesume.LastName;
+            string EmailAddress = inforesume.EmailAddress;
+            string MobileNumber = inforesume.MobileNumber;
+            string StreetName = inforesume.StreetName;
+            string City = inforesume.City;
+            string Province = inforesume.Province;
+            string Region = inforesume.Region;
 
+            //Converting Skills and Objectives
+            string Objective = inforesume.Objective;
+            string Skill1 = inforesume.Skill1;
+            string Skill2 = inforesume.Skill2;
+            string Skill3 = inforesume.Skill3;
+            string Skill4 = inforesume.Skill4;
+
+            //Converting Educational Background
+            string Tertiary = inforesume.Tertiary;
+            string Course = inforesume.Course;
+            string TertiaryYears = inforesume.TertiaryYears;
         }
     }
 }
